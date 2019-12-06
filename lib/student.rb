4246291ea6c258c7b9563628ed12f56167978d66
Micah Levine <micah.levine@outlook.com>
2019-12-06 02:58:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,4 +96,10 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_student_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
 end
